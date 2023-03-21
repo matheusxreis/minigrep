4c56e6672b2069132291df40a243d3d7d3161b34
Matheus Reis <matheus.reis081@gmail.com>
2023-03-21 20:55:33 -0300

    handling error in Config::new()

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file {}", config.filename);
@@ -28,10 +32,14 @@ struct Config<'a> {
   // it needs to know until which block it is valid
 
 impl<'a> Config<'a> {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enought arguments.");
+        }
+
         let query = &args[1];
         let filename = &args[2];
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
