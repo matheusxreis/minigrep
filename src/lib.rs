68690bd2e07c8990e86de4d3307dcd97c720c6fe
Matheus Reis <matheus.reis081@gmail.com>
2023-03-22 11:11:48 -0300

    test: creating run and Config tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 mod search;
-
+#[derive(PartialEq, Debug)]
 pub struct Config<'a> {
     pub query: &'a str,
     pub filename: &'a str,
@@ -51,4 +51,68 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Not enought arguments.")]
+    fn not_create_config_if_not_args() {
+        let args = vec![String::from("")];
+        Config::new(&args).unwrap();
+    }
+    #[test]
+    fn create_new_config_without_case_sensitive() {
+        let args = vec![
+            "bin_path".to_string(),
+            "query".to_string(),
+            "filename".to_string(),
+        ];
+
+        let assert = Config {
+            query: "query",
+            filename: "filename",
+            case_sensitive: true,
+        };
+
+        let result = Config::new(&args).unwrap();
+
+        assert_eq!(assert, result);
+    }
+
+    #[test]
+    fn file_exist() {
+        let config = Config {
+            query: "to",
+            filename: "poem.txt",
+            case_sensitive: false,
+        };
+
+        let mut result = false;
+
+        if let Ok(()) = run(&config) {
+            result = true;
+        }
+
+        assert!(result);
+    }
+
+    #[test]
+    fn file_not_exist() {
+        let config = Config {
+            query: "to",
+            filename: "not_exist_file.txt",
+            case_sensitive: false,
+        };
+
+        let mut result: bool = true;
+
+        if let Err(_) = run(&config) {
+            result = false;
+        };
+
+        assert!(!result)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-
     if let Err(e) = minigrep::run(&config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
