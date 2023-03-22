4f846c9a19d13ac72af49345c4ec6671c614dfb1
Matheus Reis <matheus.reis081@gmail.com>
2023-03-22 14:34:52 -0300

    test: testing save value in file

--- src/save/mod.rs
+++ src/save/mod.rs
@@ -2,9 +2,36 @@ use std::fs;
 use std::path::Path;
 
 pub fn save_in_file(contents: &Vec<&str>, filename: &str) {
-    println!("kjawjkas HERE{}", filename);
+    let mut value = String::new();
     for line in contents.iter() {
-        fs::write(Path::new(filename), line).unwrap()
+        value += line;
+        value += "\n";
     }
+    fs::write(Path::new(filename), value.trim()).unwrap()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::io::prelude::*;
+
+    #[test]
+    fn save_value_file() {
+        let assert = "Any value\nwrite in file\nand\nit works!";
+
+        let content = vec!["Any value", "write in file", "and", "it works!"];
+        let filename = "test.txt";
+
+        save_in_file(&content, filename);
+
+        let mut result = String::new();
+        let mut f = fs::File::open(filename).unwrap();
+        f.read_to_string(&mut result)
+            .expect("fail to read file in test");
+
+        assert_eq!(assert, result);
+
+        fs::remove_file(filename).unwrap();
+    }
+}
