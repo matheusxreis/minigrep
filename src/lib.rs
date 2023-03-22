8c23a2487a049dd4076cfd1a74b51c8e2720c6f8
Matheus Reis <matheus.reis081@gmail.com>
2023-03-22 10:04:45 -0300

    feat: creating case_insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -46,16 +46,39 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     }
     results
 }
+
+pub fn search_case_insentive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
-        let contents = "/
-        Rust:\nsafe, fast, productive.\nPick three.";
+        let contents = "/\nRust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUSt";
+        let contents = "/\nRust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insentive(query, contents)
+        )
     }
 }
